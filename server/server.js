e8bea8ee959917c042049fec87dd69ea71627eb3
asalas <67822688+andrewjsalas@users.noreply.github.com>
2023-06-13 10:39:14 -0700

    Added testing link

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
 
 const mongoURI = process.env.MongoDB_URI;
+const mongoLocalHost = process.env.MongoDB_localhost; // For testing
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
